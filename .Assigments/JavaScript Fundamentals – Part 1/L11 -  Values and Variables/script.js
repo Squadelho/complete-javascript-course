f6a0073488aebf4ca0b04b4bd320728599283cd9
Squadelho <81372361+Squadelho@users.noreply.github.com>
2022-06-02 20:41:10 +0000

    Assigment L10

---
+++ .Assigments/JavaScript Fundamentals – Part 1/L11 -  Values and Variables/script.js
@@ -0,0 +1,7 @@
+let country = "Portugl";
+let continent = "Europe";
+let population = "10M";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,15 +2,9 @@ let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 let firstName = "Hugo";
-<<<<<<< HEAD
 let myFirtsJob = "Comercial";
-=======
->>>>>>> f32c2a3231f17024967e854453d242a1754ef240
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-<<<<<<< HEAD
 console.log(myFirtsJob);
-=======
->>>>>>> f32c2a3231f17024967e854453d242a1754ef240
