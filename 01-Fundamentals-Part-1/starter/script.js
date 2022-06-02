fb6304a69f06c29664b75761a177d1b6fa856c51
Squadelho <81372361+Squadelho@users.noreply.github.com>
2022-06-02 21:08:37 +0000

    S2L12 - Data Type

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,8 @@
-let js = "amazing";
+//coment line
+/* coment multiple lines */
+
+/*
+ let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 let firstName = "Hugo";
@@ -7,4 +11,32 @@ let myFirtsJob = "Comercial";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-console.log(myFirtsJob);
+console.log(myFirtsJob); 
+*/
+
+//---------------------------------------------------------------------------
+
+/* Primitive Data types:
+Number - Floating point numbers -> used for decimals and interger: leat age = 23;
+String - Sequence of characters -> used for text: let firstName = "Jonas";
+Boolean - Logical type that can only be true or false -> used for taking decisions: let fullAge = true; */
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Hugo");
+
+javascriptIsFun = "yes"; //mudar o valor da variavel
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+//undefined variables
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1990;
+console.log(typeof year);
+
+console.log(typeof null); //BUG!!!
