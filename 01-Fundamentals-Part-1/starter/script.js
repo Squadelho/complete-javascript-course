441c4d3ce6320354acaf730cfc25019de6242434
Squadelho <81372361+Squadelho@users.noreply.github.com>
2022-06-06 21:44:57 +0000

    S2L18 -Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -134,4 +134,30 @@ using tempplate literals`); //criar multi-lines com o return (enter)
 
 ///////////////////////////////////////////////////////////////////
 
-//      L18 -
+//      L18 -Taking Decisions: if / else Statements
+
+/* 
+const age = 19;
+
+if (age >= 18) {
+  console.log(`Sarah can start driving license 🚑`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years!`);
+}
+
+const birthYear = 1999;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century); 
+*/
+
+///////////////////////////////////////////////////////////////////
+
+//      L20 -
